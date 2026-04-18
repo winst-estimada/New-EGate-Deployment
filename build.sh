c9d00ce2c23a1ecb091d0b904153d7aea82fe781
EGate Project <wn.estimada@gmail.com>
2026-04-18 12:39:23 +0800

    build

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    python manage.py createsuperuser --noinput || true
+    python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); u=User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); u.is_admin=True; u.is_staff=True; u.is_superuser=True; u.save()"
+fi
